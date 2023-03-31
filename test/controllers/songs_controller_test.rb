81e068b768df400fc668fba8d3901e05c70f87ba
Chazz Gagliano <cgags17285@gmail.com>
2023-03-31 14:53:23 -0400

    update action added

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -24,8 +24,14 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
     assert_equal ["title", "album", "artist", "year"], data.keys
   end
 
-  
+  test "update" do
+    song = Song.first
+    patch "/songs/#{song.id}.json", params: { title: "Updated title" }
+    assert_response 200
 
+    data = JSON.parse(response.body)
+    assert_equal "Updated title", data["title"]
+  end
    
   
 
