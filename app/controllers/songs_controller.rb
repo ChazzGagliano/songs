de2968c0a42b18eeed5c3fb356d80d3b936c5428
Chazz Gagliano <cgags17285@gmail.com>
2023-03-31 14:43:20 -0400

    update/destroy action added

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,4 +21,21 @@ class SongsController < ApplicationController
     render :show
   end
 
+  def update
+    @song = Song.find_by(id: params[:id]) 
+    @song.title = params[:title] || @song.title
+    @song.album = params[:album] || @song.album
+    @song.artist = params[:artist] || @song.artist
+    @song.year = params[:year] || @song.year
+    
+    @song.save
+    render :show
+  end
+
+  def destroy 
+    @song = Song.find_by(id: params[:id])
+    @song.destroy
+    render json: {message: "song destroyed"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/songs/:id" => "songs#show"
   get "/songs" => "songs#index"
   post "/songs" => "songs#create"
+  patch "/songs/:id" => "songs#update"
+  delete "/songs/:id" => "songs#destroy"
 end
